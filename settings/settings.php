788c557f0ec79fa835c76a82866da1282ec9ad79
Ifitskie <ifitskie@gmail.com>
2016-10-06 14:09:41 +0200

    Update settings.php

--- settings/settings.php
+++ settings/settings.php
@@ -5,11 +5,5 @@
 //$db_password = '';
 //$db_name = 'spotify';
 
-$db_host = 'localhost';
-$db_username = '0892682';
-$db_password = 'Kroko1loko!';
-$db_name = '0892682';
-
-
 // conectie met database
 $db = mysqli_connect($db_host, $db_username, $db_password, $db_name);
